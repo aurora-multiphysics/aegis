d50d59e256398cd850b6509b5265904671afb75f
Waqar Butt <waqar.butt@ukaea.uk>
2022-11-02 15:58:00 +0000

    updated damgc_call with new geometry test

--- src/dagmc_call/dagmc_call.cpp
+++ src/dagmc_call/dagmc_call.cpp
@@ -29,73 +29,97 @@ using moab::DagMC;
 using moab::OrientedBoxTreeTool;
 moab::DagMC* DAG;
 
-static const char input_file[] = "triple_block.h5m";
+static const char input_file[] = "big_pipe.h5m";
 
 int main() {
   DAG = new DagMC();
-  DAG->load_file(input_file); // open test dag file 
-  DAG->init_OBBTree();
-  DagMC::RayHistory history;
-  int vol_idx = 1;
-  int n_surfs = DAG->num_entities(2);
-  int n_vols = DAG->num_entities(3);
+  DAG->load_file(input_file); // open test dag file
+  DAG->init_OBBTree(); // initialise OBBTree 
+  DagMC::RayHistory history; // initialise RayHistory object
+  int vol_idx = 1; 
+  int n_surfs = DAG->num_entities(2); // No. of surfaces in loaded geometry
+  int n_vols = DAG->num_entities(3); // No. of volumes in loaded geometry
   std::cout << "No. of surfaces - " << n_surfs << std::endl;
   std::cout << "No. of Volumes - " << n_vols << std::endl;
   EntityHandle vol_h = DAG->entity_by_index(3, vol_idx);
-  double dir[3] = {1.0, 1.0, 0.0};       // ray along x direction
-  double origin[3] = {-5.0, 0.0, 0.0};  // origin at 0 0 0
-  double next_surf_dist;
-  EntityHandle next_surf;
-  int invol;
-  int nrayfire = 1;
-  double ray_start[3];
-  EntityHandle vol_n;
-  double angle[3];
-  EntityHandle* nearest_surf;
-  double result;
+  double dir[3] = {1.0, 0.0, 1.0};       // ray launch direction
+  double origin[3] = {-6.0, 0.0, -6.0};  // ray launch origin
+  double next_surf_dist=0.0; // distance to the next surface ray will intersect
+  double prev_surf_dist; // previous next_surf_dist before the next ray_fire call
+  EntityHandle next_surf; // surface id 
+  int invol; // in volume result
+  int nrayfire=0; // No. of ray_fire calls 
+  EntityHandle prev_surf; // previous surface id
 
-  std::ofstream ray_coords;
-  ray_coords.open("ray_coords.txt");
+  std::ofstream ray_coords; // define stream ray_coords
+  ray_coords.open("ray_coords.txt"); // write out stream ray_coords to "ray_coords.txt" file 
 
-  ray_coords << origin[0] << ' ' << origin[1] << ' ' << origin[2] << ' ' << std::endl; 
+  ray_coords << origin[0] << ' ' << origin[1] << ' ' << origin[2] << ' ' << std::endl; // store first ray origin
 
   // check if in volume before ray_fire
-  DAG->point_in_volume(vol_h, origin, invol); 
+  DAG->point_in_volume(vol_h, origin, invol);
   std::cout << invol << " = in volume (1 for yes, 0 for no)" << std::endl;
   std::cout << "Volume ID - " << vol_h << std::endl;
-  history.reset();
+  history.reset(); // reset history before launching any rays
 
+  // launch
+  DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+  nrayfire +=1;
+  for (int i=0; i<3; ++i) { // calculate next ray launch point
+    origin[i] = origin[i] + (next_surf_dist * dir[i]);
+    ray_coords << origin[i] << ' '; // write out new ray origin to file
+  }
+    ray_coords << std::endl;
+    std::cout << std::endl;
+    std::cout << "Distance to next surface = " << next_surf_dist << std::endl;
+    std::cout << "Next Surface id - " << next_surf << std::endl;
+    DAG->next_vol(next_surf, vol_h, vol_h); // move to next volume id (determined from next_surf)
+    DAG->point_in_volume(vol_h, origin, invol);
+    std::cout << "Volume ID - " << vol_h << std::endl;
+    std::cout << invol << " = in volume (1 for yes, 0 for no)" << std::endl;
 
-  while (nrayfire < 10){
-
+    // launch
+    DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+    nrayfire +=1;
     for (int i=0; i<3; ++i) { // loop to calculate next ray launch point
-      origin[i] = origin[i] + (next_surf_dist * dir[i]); 
+      origin[i] = origin[i] + (next_surf_dist * dir[i]);
       ray_coords << origin[i] << ' ';
     }
     ray_coords << std::endl;
     std::cout << std::endl;
-
-    std::cout << "Firing ray" << std::endl;
-    DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
-    DAG->point_in_volume(vol_h, origin, invol); 
-    std::cout << invol << " = in volume (1 for yes, 0 for no)" << std::endl;
+    DAG->point_in_volume(vol_h, origin, invol);
+    std::cout << "Volume ID - " << vol_h << std::endl;
     std::cout << "Distance to next surface = " << next_surf_dist << std::endl;
     std::cout << "Next Surface id - " << next_surf << std::endl;
-    DAG->get_angle(next_surf, NULL, angle, &history);
-    std::cout << "Angle calculated from get_angle - ";
-    for (int i=0; i<3; ++i) { // loop to calculate next ray launch point
-      std::cout << angle[i] << " ";
-    }
-    if (next_surf == 0) {
-      DAG->next_vol(next_surf, vol_h, vol_h);
-      //std::cout << "Ran next_vol" <<std::endl;
-      dir[1]=-dir[1]; 
-      }
-    std::cout << "Next Volume ID - " << vol_h << std::endl; 
-    nrayfire += 1;
-    
-  }
+    DAG->point_in_volume(vol_h, origin, invol);
+    std::cout << "Volume ID - " << vol_h << std::endl;
+    std::cout << invol << " = in volume (1 for yes, 0 for no)" << std::endl;
 
-  std::cout << "No. of ray_fire calls - " << nrayfire << std::endl;
+  while (next_surf !=0){
+    dir[0]=-dir[0]; // flip Z-component to keep ray in pipe
+    prev_surf = next_surf;
+    prev_surf_dist = next_surf_dist;
+    DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+    nrayfire +=1;
+    if (next_surf_dist < 1e-3){ // fix distance if too small 
+      next_surf_dist = prev_surf_dist;
+    }  
+    for (int i=0; i<3; ++i) { // loop to calculate next ray launch point
+      origin[i] = origin[i] + (next_surf_dist * dir[i]);
+      ray_coords << origin[i] << ' ';
+    }
+    ray_coords << std::endl;
+    std::cout << std::endl;
+    std::cout << "Distance to next surface = " << next_surf_dist << std::endl;
+    std::cout << "Next Surface id - " << next_surf << std::endl;
+    if (prev_surf == next_surf){
+      DAG->next_vol(next_surf, vol_h, vol_h);
+      DAG->point_in_volume(vol_h, origin, invol);
+      std::cout << "Volume ID - " << vol_h << std::endl;
+      std::cout << invol << " = in volume (1 for yes, 0 for no)" << std::endl;
+    }
+
+  }
+  std::cout << "No. of ray_fire calls - " << nrayfire << std::endl; // print the No. of ray_fire calls
     return 0;
 }
--- test/unit/dagmc_call_test.cpp
+++ test/unit/dagmc_call_test.cpp
@@ -61,3 +61,57 @@ TEST_F(DagmcSimpleTest, Triple_Block_rayfire) {
   EXPECT_NEAR(expected_next_surf_dist, next_surf_dist, eps);
 }
 
+
+TEST_F(DagmcSimpleTest, Ray_Propagation_Pipe) {
+  DAG = new DagMC();
+  DAG->load_file("big_pipe.h5m"); // open test dag file 
+  DAG->init_OBBTree();
+  DagMC::RayHistory history;
+  int vol_idx = 1;
+  EntityHandle vol_h = DAG->entity_by_index(3, vol_idx);
+  double dir[3] = {1.0, 0.0, 1.0};       // ray launch direction
+  double origin[3] = {-6.0, 0.0, -6.0};  // ray launch origin
+  double next_surf_dist;
+  int surfaces_intersected;
+  int expected_surfaced_intersected;
+  EntityHandle next_surf;
+  int nrayfire=0; // No. of ray_fire calls 
+  EntityHandle prev_surf; // previous surface id
+  double prev_surf_dist; // previous next_surf_dist before the next ray_fire call
+
+  history.reset(); // reset history before launching any rays
+
+  // launch ray
+  DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+  nrayfire +=1;
+  for (int i=0; i<3; ++i) { // calculate next ray launch point
+    origin[i] = origin[i] + (next_surf_dist * dir[i]);
+  }
+  DAG->next_vol(next_surf, vol_h, vol_h); // move to next volume id (determined from next_surf)
+  
+
+  // launch
+  DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+  nrayfire +=1;
+  for (int i=0; i<3; ++i) { // calculate next ray launch point
+    origin[i] = origin[i] + (next_surf_dist * dir[i]);
+  }
+
+  while (next_surf !=0){
+    dir[0]=-dir[0]; // flip Z-component to keep ray in pipe
+    prev_surf = next_surf;
+    prev_surf_dist = next_surf_dist;
+    DAG->ray_fire(vol_h, origin, dir, next_surf, next_surf_dist, &history, 0, 1);
+    nrayfire +=1;
+    if (next_surf_dist < 1e-3){ // fix distance if too small 
+      next_surf_dist = prev_surf_dist;
+    }  
+    for (int i=0; i<3; ++i) { // loop to calculate next ray launch point
+      origin[i] = origin[i] + (next_surf_dist * dir[i]);
+    }
+    if (prev_surf == next_surf){
+      DAG->next_vol(next_surf, vol_h, vol_h);
+    }
+  EXPECT_EQ(nrayfire, 8);
+  }
+}
