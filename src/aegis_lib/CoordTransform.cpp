fcb4096b9e0cb0b2287e11f4cce9cde2dc590253
Waqar Butt <waqar.butt@ukaea.uk>
2024-06-14 14:30:31 +0100

    added Position3D class

--- src/aegis_lib/CoordTransform.cpp
+++ src/aegis_lib/CoordTransform.cpp
@@ -91,3 +91,127 @@ CoordTransform::polar_to_flux(const double & e0, const double & e1, const double
   std::vector<double> outputVector = {-psi, theta, phi};
   return outputVector;
 }
+
+// Position 3D methods
+
+Position3D &
+Position3D::operator+=(Position3D other)
+{
+  _e0 += other._e0;
+  _e1 += other._e1;
+  _e2 += other._e2;
+  return *this;
+}
+
+Position3D &
+Position3D::operator+=(double v)
+{
+  _e0 += v;
+  _e1 += v;
+  _e2 += v;
+  return *this;
+}
+
+Position3D &
+Position3D::operator-=(Position3D other)
+{
+  _e0 -= other._e0;
+  _e1 -= other._e1;
+  _e2 -= other._e2;
+  return *this;
+}
+
+Position3D &
+Position3D::operator-=(double v)
+{
+  _e0 -= v;
+  _e1 -= v;
+  _e2 -= v;
+  return *this;
+}
+
+Position3D &
+Position3D::operator*=(double v)
+{
+  _e0 *= v;
+  _e1 *= v;
+  _e2 *= v;
+  return *this;
+}
+
+Position3D &
+Position3D::operator/=(double v)
+{
+  _e0 /= v;
+  _e1 /= v;
+  _e2 /= v;
+  return *this;
+}
+
+// update coordinates without changing system
+void
+Position3D::update_coords(double e0New, double e1New, double e2New)
+{
+  _e0 = e0New;
+  _e1 = e1New;
+  _e2 = e2New;
+}
+
+// update coordinates with a new coordinate system. Provide coordinates of new system.
+void
+Position3D::update_coords(double e0New, double e1New, double e2New, coordinateSystem newCoordSys)
+{
+  _e0 = e0New;
+  _e1 = e1New;
+  _e2 = e2New;
+  _coordSys = newCoordSys;
+}
+
+// Position 3D coordTransforms
+
+Position3D
+CoordTransform::cart_to_polar(const Position3D & pos)
+{
+  double x = pos[0];
+  double y = pos[1];
+  double z = pos[2];
+  double r = sqrt(pow(x, 2) + pow(y, 2)); // calculate
+  double phi = atan2(-y, x);              // calculate phi
+  Position3D newPos(r, z, phi, coordinateSystem::POLAR);
+  return newPos;
+}
+
+Position3D
+CoordTransform::polar_to_cart(const Position3D & pos)
+{
+  double r = pos[0];
+  double z = pos[1];
+  double phi = pos[2];
+  double x = r * cos(phi);  // calculate x
+  double y = -r * sin(phi); // calculate y
+  Position3D newPos(x, y, z, coordinateSystem::CARTESIAN);
+  return newPos;
+}
+
+Position3D
+CoordTransform::polar_to_flux(Position3D & pos, const std::shared_ptr<EquilData> & equilibrium)
+{
+  // transform to polar before flux
+  if (pos.coordSys() == coordinateSystem::CARTESIAN)
+  {
+    pos = CoordTransform::cart_to_polar(pos);
+  }
+
+  double r = pos[0];
+  double z = pos[1];
+  double phi = pos[2];
+  double psi =
+      alglib::spline2dcalc(equilibrium->psiSpline, r, z); // spline interpolation of psi(R,Z)
+  double theta = atan2(z - equilibrium->zcen, r - equilibrium->rcen);
+  if (theta < -M_PI_2)
+  {
+    theta = 2 * M_PI + theta;
+  }
+  Position3D newPos(-psi, theta, phi, coordinateSystem::FLUX);
+  return newPos;
+}
--- src/aegis_lib/CoordTransform.h
+++ src/aegis_lib/CoordTransform.h
@@ -8,11 +8,107 @@
 #include "EquilData.h"
 #include "alglib/interpolation.h"
 
+class Position3D : public AegisBase
+{
+  public:
+  Position3D() = default;
+  template <typename T>
+  Position3D(const T coords, coordinateSystem coordSys) : _e0(coords[0]), _e1(coords[1]), _e2(coords[2]), _coordSys(coordSys) {};
+  Position3D(double e0, double e1, double e2, coordinateSystem coordSys) : _e0(e0), _e1(e1), _e2(e2), _coordSys(coordSys) {};
+
+
+  // return current coordinate system 
+  coordinateSystem coordSys() { return _coordSys; } 
+
+  // return STL array of coords
+  std::array<double, 3> coords() { return {_e0, _e1, _e2}; } 
+
+  // return STL vector of coords
+  std::vector<double> coords_vec() { return {_e0, _e1, _e2}; }
+
+  // update coordinates without changing system 
+  template <typename T> void update_coords(T newCoords)
+  {
+    _e0 = newCoords[0]; 
+    _e1 = newCoords[1];
+    _e2 = newCoords[2]; 
+  }
+
+  // update coordinates without changing system
+  void update_coords(double e0New, double e1New, double e2New);
+  
+  // update coordinates with a new coordinate system. Provide coordinates of new system.
+  template <typename T> void update_coords(T newCoords, coordinateSystem newCoordSys)
+  {
+    _e0 = newCoords[0]; 
+    _e1 = newCoords[1];
+    _e2 = newCoords;
+    _coordSys = newCoordSys; 
+  }
+
+  // update coordinates with a new coordinate system. Provide coordinates of new system.
+  void update_coords(double e0New, double e1New, double e2New, coordinateSystem newCoordSys);
+
+  // generalised coordinates
+  double _e0 = 0.0; // first general coordinate
+  double _e1 = 0.0 ; // second general coordinate 
+  double _e2 = 0.0; // third general coordinate
+
+
+  // operator overloading
+  // template <typename T> 
+  // Position3D& operator=(T other)
+  // {
+  //   _e0 = other[0];
+  //   _e1 = other[1];
+  //   _e2 = other[2];
+  //   return *this;
+  // }
+
+  Position3D& operator+=(Position3D);
+  Position3D& operator+=(double v);
+  Position3D& operator-=(Position3D);
+  Position3D& operator-=(double v);
+  Position3D& operator*=(double v);
+  Position3D& operator/=(double v);
+
+  const double& operator[](int i) const
+  {
+    switch (i) {
+    case 0:
+      return _e0;
+    case 1:
+      return _e1;
+    case 2:
+      return _e2;
+    default:
+      throw std::out_of_range {"Index in Position must be between 0 and 2."};
+    }
+  }
+  double& operator[](int i)
+  {
+    switch (i) {
+    case 0:
+      return _e0;
+    case 1:
+      return _e1;
+    case 2:
+      return _e2;
+    default:
+      throw std::out_of_range {"Index in Position must be between 0 and 2."};
+    }
+  }
+
+
+  protected:
+
+  private:
+  coordinateSystem _coordSys = coordinateSystem::CARTESIAN;
+};
+
 
 namespace CoordTransform
 {
-
-
 // Cartesian to polar toroidal (direction = "backwards" for polar->cartesian)
 std::vector<double> cart_to_polar(const std::vector<double> &xyz);
 
@@ -24,6 +120,14 @@ inline std::vector<double> cart_to_polar_xy(const std::vector<double> &xyz)
   return rz;
 };
 
+inline Position3D cart_to_polar_xy(const Position3D &pos)
+{
+  double r = sqrt(pow(pos[0], 2) + pow(pos[1], 2));
+  double z = pos[2];
+  Position3D newPos(r, z, 0.0, coordinateSystem::POLAR);
+  return newPos;
+}
+
 // get polar angle (PHI) from (X,Y)
 inline double cart_to_polar_phi(const std::vector<double> &xyz)
 {
@@ -31,6 +135,9 @@ inline double cart_to_polar_phi(const std::vector<double> &xyz)
   return phi;
 };
 
+Position3D cart_to_polar(const Position3D & pos);
+Position3D polar_to_cart(const Position3D & pos);
+Position3D polar_to_flux(Position3D & pos, const std::shared_ptr<EquilData> & equilibrium);
 
 std::vector<double> cart_to_polar(const double &e0, const double &e1, const double &e2);
 
@@ -41,9 +148,7 @@ std::vector<double> polar_to_cart(const double &e0, const double &e1, const doub
 std::vector<double> polar_to_flux(const std::vector<double> &inputVector,
                                   const std::shared_ptr<EquilData>& equilibrium);
 
-
 std::vector<double> polar_to_flux(const double &e0, const double &e1, const double &e2, const std::shared_ptr<EquilData>& equilibrium);
-
 };
 
 #endif
